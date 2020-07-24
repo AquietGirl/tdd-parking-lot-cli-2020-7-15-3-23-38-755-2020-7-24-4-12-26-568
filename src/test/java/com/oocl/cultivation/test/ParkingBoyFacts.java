33b0dd34e6ddf6589d268ec8f580f59cc94ce2f5
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-24 16:17:59 +0800

    [PackingBoyFacts] [Alisa] test:should_return_ticket_when_boy_parking_car

--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -1,10 +1,23 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.ParkingBoy;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParkingBoyFacts {
     @Test
-    void write_your_first_test() {
+    void should_return_ticket_when_boy_parking_car() {
+        //given
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy();
 
+        //when
+        String ticket = parkingBoy.parkingCar(car);
+
+        //then
+        Assertions.assertEquals("ticket", ticket);
     }
 }
