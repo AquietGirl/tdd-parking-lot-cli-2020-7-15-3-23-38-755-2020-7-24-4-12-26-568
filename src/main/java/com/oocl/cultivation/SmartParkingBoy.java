683619d6ca024e35a069d66eab05c72374f72a84
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-26 01:15:06 +0800

    [PackingBoyFacts] [Alisa] pass: should_return_car_in_park_that_have_more_position_when_boy_parking_car_given_car

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -1,13 +1,21 @@
 package com.oocl.cultivation;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SmartParkingBoy extends ParkingBoy {
     @Override
     public Ticket parkingCar(Car car) {
+       if (isCanParkingCar(car)) {
+            List<Park> parks = this.getParkList().stream().sorted(Comparator.comparing(Park::getEmptyPositionCount).reversed()).collect(Collectors.toList());
+            Park morePositionPark = parks.get(0);
+            Ticket ticket = new Ticket(car.getCarNumber(), car, morePositionPark);
+            this.getTicketList().add(ticket);
+            morePositionPark.setEmptyPositionCount(morePositionPark.getEmptyPositionCount() - 1);
+            return ticket;
+        }
         return null;
     }
 
-    @Override
-    public Car fetchingCar(Ticket ticket) {
-        return null;
-    }
 }
