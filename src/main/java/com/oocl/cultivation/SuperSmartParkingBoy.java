ee339f35e1372da4f360975ae333045c53933cc3
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-26 14:50:59 +0800

    [PackingBoyFacts] [Alisa] pass : should_return_car_in_park_that_have_larger_available_position_rate

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -1,8 +1,28 @@
 package com.oocl.cultivation;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class SuperSmartParkingBoy extends ParkingBoy {
     @Override
     public Ticket parkingCar(Car car) {
+        if (isCanParkingCar(car)) {
+            Optional<Park> parks = this.getParkList().stream().reduce((x, y) -> {
+                int rateX = (x.getEmptyPositionCount() / 10);
+                int rateY = (y.getEmptyPositionCount() / 10);
+                return rateX > rateY ? x : y;
+            });
+            if (parks.isPresent()){
+                Park morePositionPark = parks.get();
+                Ticket ticket = new Ticket(car.getCarNumber(), car, morePositionPark);
+                this.getTicketList().add(ticket);
+                morePositionPark.setEmptyPositionCount(morePositionPark.getEmptyPositionCount() - 1);
+                return ticket;
+            }
+
+        }
         return null;
     }
 }
